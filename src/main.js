df8bfcbd890aefffd521435122e84a360b7c288a
LianQianChao <i@lchao.cc>
2022-03-08 19:47:29 +0800

    element-plus按需引入

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router'
-import ElementPlus from 'element-plus'
+import {ElButton} from 'element-plus'
 
 import 'element-plus/lib/theme-chalk/index.css';
 
 
 const app = createApp(App)
 app.use(router)
-app.use(ElementPlus)
+app.use(ElButton)
 app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import vitePluginImport from 'vite-plugin-babel-import'
 
 // https://vitejs.dev/config/
 export default ({mode}) => defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+  vitePluginImport([
+    {
+      libraryName: 'element-plus',
+      libraryDirectory: 'es',
+      style(name){
+        return `element-plus/lib/theme-chalk/${name}.css`;
+      }
+    }
+  ])],
   resolve: {
     alias: {
       '~': path.resolve(__dirname, './'),
