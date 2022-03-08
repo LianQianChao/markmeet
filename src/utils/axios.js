f164fe178b73093a30bb15feb0e2cc5f3b56ae4d
LianQianChao <i@lchao.cc>
2022-03-08 20:18:53 +0800

    在src/utils目录创建index.js并封装localStorage get/set/remove方法

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 import router from '@/router/index'
 import config from '~/config'
+import {ElMessage} from 'element-plus'
+import {localGet} from './index'
 
 axios.defaults.baseURL = config[import.meta.env.MODE].baseURL;
 
@@ -8,18 +10,18 @@ axios.defaults.withCredentials = true;
 
 axios.defaults.headers['X-Requested-With'] = 'XMLHttpRequest';
 
-axios.defaults.headers['token'] = localStorage.getItem('token') || '';
+axios.defaults.headers['token'] = localGet('token') || '';
 
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 axios.interceptors.response.use(res => {
     if (typeof res.data !== 'object'){
-        alert('服务端异常！')
+        ElMessage.error('服务端异常！')
         return Promise.reject(res);
     }
     if(res.data.resultCode != 200){
         if(res.data.message){
-            alert(res.data.message);
+            ElMessage.error(res.data.message);
         }
         if(res.data.resultCode == 419){
             router.push({
