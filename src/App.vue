ae6e918afbfc7e3a3f1504474d7ea4d52483bc9f
LianQianChao <i@lchao.cc>
2022-03-29 02:36:02 +0800

    feat:编写login.vue页面

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,3 @@
-<script setup>
-
-</script>
-
 <template>
   <router-view></router-view>
 </template>
@@ -11,8 +7,8 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+body{
+  margin: 0 0;
 }
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,91 @@
 <script>
+import { reactive } from 'vue'
+
 export default {
-    
+  setup() {
+    const formData = reactive({
+      username: '',
+      password: '',
+      remember: false,
+    })
+    const onSubmit = () => {
+      console.log(formData)
+    }
+
+    return {
+      formData,
+      onSubmit,
+    }
+  },
 }
 </script>
 <template>
-    <div>
-        登录
+  <div class="main">
+    <div class="content">
+      <div class="login-body">
+        <el-form :model="formData" label-width="120px" label-position="top">
+          <el-form-item label="用户名">
+            <el-input v-model="formData.username" />
+          </el-form-item>
+
+          <el-form-item label="密码">
+            <el-input v-model="formData.password" />
+          </el-form-item>
+
+          <el-form-item>
+            <el-button type="primary" @click="onSubmit">登录</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+      <div>
+          <h4>欢迎登录</h4>
+          <img src="../assets/logo.png">
+      </div>
     </div>
+  </div>
 </template>
+<style scoped>
+.main {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background: linear-gradient(to left, #1d3b67 50%, #f4f6f9 50%);
+}
+.login-body {
+  width: 400px;
+  height: 420px;
+  border-radius: 6px;
+  background-color: #f5f6f9;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.login-body + div {
+    text-align: center;
+  width: 400px;
+  height: 420px;
+  background-color: #203c67;
+  font-size: 30px;
+  color: white;
+}
+.login-body > form {
+  width: 270px;
+  border: 1px solid #edefef;
+  border-radius: 6px;
+  background-color: #ffffff;
+  padding: 34px 34px;
+}
+.content {
+  height: 420px;
+  background: #f4f6f9;
+  border-radius: 6px;
+  box-shadow: 0px 0px 10px #222121;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.login-body > form .el-button--primary {
+  width: 100%;
+}
+</style>
