04a060507079fc812209b34ab59c3f8e4f705097
LianQianChao <i@lchao.cc>
2022-03-29 16:06:38 +0800

    feat:实现登录

--- mock/login/login.js
+++ mock/login/login.js
@@ -1,11 +1,20 @@
 import Mock from 'mockjs'
+import {success,failure} from '../utils/result'
+
+
+const loginResult = {
+    token: 'tokeljdjofdafljlbdjljie'
+}
 
 const result = ops =>{
-    console.log(ops)
-    return{
-        username:'admin',
-        password: '123434'
+    
+    const body = JSON.parse(ops.body)
+    if(body.username === 'admin' && body.password === 'admin'){
+        return success(200,'登录成功',loginResult)
+    }else{
+        return failure(400,'登录失败')
     }
+    
 }
 
 Mock.mock('/mock/login','get',result)
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,20 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: '/mock',
-    timeout: 1000
+  baseURL: '/mock',
+  timeout: 1000,
 })
+
+instance.get = (url, data) => {
+  return new Promise(function (resolve, reject) {
+    instance({
+      method: 'get',
+      url: url,
+      data: data,
+    })
+      .then(res => resolve(res.data))
+      .catch(error => reject(error))
+  })
+}
 
 export default instance
