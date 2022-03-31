aa4ee93d90e94677a9e02ac203bf9c21355bc612
LianQianChao <i@lchao.cc>
2022-03-31 23:13:38 +0800

    feat:首页布局容器

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,39 @@
+<script>
+import { ref } from 'vue'
+import router from './router'
+
+export default {
+  setup() {
+    const size = ref(0)
+    const show = ref(false)
+    router.afterEach(to => {
+      if (to.name !== 'login') {
+        size.value = 20
+        show.value = true
+      }
+    })
+    return {
+      size,
+      show,
+    }
+  },
+}
+</script>
 <template>
-  <router-view></router-view>
+  <div class="common-layout">
+    <el-container>
+      <el-header v-if="show">Header</el-header>
+      <el-container>
+        <el-aside v-if="show" width="200px">Aside</el-aside>
+        <el-container>
+          <el-main :style="{ padding: size }"
+            ><router-view></router-view
+          ></el-main>
+          <el-footer v-if="show">Footer</el-footer>
+        </el-container>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <style>
@@ -8,7 +42,7 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
-body{
+body {
   margin: 0 0;
 }
 </style>
